0afe5a23d7f06825b1d69ee0cfdd650447697966
Czy <277171229@qq.com>
2020-07-27 11:07:55 +0800

    no message

--- pages/quizzes/detail.js
+++ pages/quizzes/detail.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-
+    detail:''
   },
   onLoad: function (option) {
     console.log(option, 'id')
@@ -14,6 +14,9 @@ Page({
   getDetail(id) {
 		networkApi.quizzes_v1.detail({id}).then((res) => {
       console.log(res, '获取题目详情');
+      this.setData({
+        detail:res.data
+      })
     }).catch(err => {
       console.log(err,'err')
     }).finally(() => {
